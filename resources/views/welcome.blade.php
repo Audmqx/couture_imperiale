03059e6431880cea6d0b9ac31fd7d9a2d574bdfc
MaximPixodeo <89903149+MaximPixodeo@users.noreply.github.com>
2023-02-14 15:13:14 +0100

    add lunar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,24 @@
         </style>
     </head>
     <body class="antialiased">
+
+        @php
+            
+            $product = \Lunar\Models\Product::find(1);
+
+            $product->getMedia('images');
+            $test = Lunar\Models\Url::whereElementType(\Lunar\Models\Collection::class)->first()?->element;
+
+            $products = $test->products()->get();
+
+            $saleImages = $products->map(function ($product) {
+                return $product->thumbnail;
+            });
+            dd($saleImages );
+            dd($product);
+        @endphp
+
+
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
